8f23519d27cd2079ae7ea9f4be5ae549cea24646
Mike Bell <hello@mikebell.io>
2020-08-21 09:51:15 +0100

    Add timestamp to script

--- backup-s3/run.sh
+++ backup-s3/run.sh
@@ -4,6 +4,10 @@ KEY=`jq -r .awskey /data/options.json`
 SECRET=`jq -r .awssecret /data/options.json`
 BUCKET=`jq -r .bucketname /data/options.json`
 
+now="$(date +'%d/%m/%Y - %H:%M:%S')"
+
+echo $now
+
 aws configure set aws_access_key_id $KEY
 aws configure set aws_secret_access_key $SECRET
 
