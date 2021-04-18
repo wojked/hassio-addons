47e071b650e7181ee6a9684659ecfa264d212f10
Alexander Babel <alexander.babel@icloud.com>
2021-04-18 15:26:06 +0200

    feat(run.sh): support names from snapshot for aws prefix and key

--- backup-s3/run.sh
+++ backup-s3/run.sh
@@ -1,16 +1,91 @@
-#!/bin/sh
+#!/bin/bash
 
-KEY=`jq -r .awskey /data/options.json`
-SECRET=`jq -r .awssecret /data/options.json`
-BUCKET=`jq -r .bucketname /data/options.json`
+#### VARIABLES ####
 
-now="$(date +'%d/%m/%Y - %H:%M:%S')"
+CONFIG_PATH=/data/options.json
 
-echo $now
+KEY=$(bashio::config 'awskey')
+SECRET=$(bashio::config 'awssecret')
+BUCKET=$(bashio::config 'bucketname')
+USE_NAME=$(bashio::config 'usename')
 
-aws configure set aws_access_key_id $KEY
-aws configure set aws_secret_access_key $SECRET
+BACKUP_PATH="/backup"
+SYMLINKS_PATH="/symlinks"
+SNAPSHOT_FILE="snapshot.json"
+SNAPSHOT_FILE_PATH="$BACKUP_PATH/$SNAPSHOT_FILE"
 
-aws s3 sync /backup/ s3://$BUCKET/ --quiet
+JQ_NAME=".name" # format: "PREFIX: KEY"
 
-echo "Done"
+#### END VARIABLES ####
+
+
+#### FUNCTIONS ####
+
+log() {
+  now="$(date +'%d/%m/%Y - %H:%M:%S')"
+  echo "$now |  ---> $1"
+}
+
+get_prefix() {
+  IFS="$2"                   # delimiter
+  read -ra ADDR <<< "$1"     # $1 is read into an array as tokens separated by IFS
+  IFS=' '                    # reset to default value after usage
+  echo "${ADDR[0]}"
+}
+
+cleanup() {
+  rm -f "$SNAPSHOT_FILE_PATH*"
+  rm -rf "$SYMLINKS_PATH"
+}
+
+create_symlinks() {
+  for filename in "$BACKUP_PATH"/*.tar; do
+    tar -zxvf "$filename" $SNAPSHOT_FILE
+
+    name=$(jq -r $JQ_NAME $SNAPSHOT_FILE_PATH)
+    prefix=$(get_prefix "$name" ':')
+    cut_length=$((${#prefix} + 1))
+
+    # Ignore files without prefix
+    if [[ "$cut_length" == 1 ]]; then
+      rm -f $SNAPSHOT_FILE_PATH
+      continue
+    fi
+
+    # Create Symlink
+    mkdir -p "$SYMLINKS_PATH/$prefix"
+    ln -s "$BACKUP_PATH/$filename" "$SYMLINKS_PATH/$prefix/${name:cut_length}.tar"
+
+    # Cleanup
+    rm -f $SNAPSHOT_FILE_PATH
+  done
+}
+
+#### END FUNCTIONS ####
+
+
+log "Starting Sync"
+
+log "Configuring AWS credentials"
+aws configure set aws_access_key_id "$KEY"
+aws configure set aws_secret_access_key "$SECRET"
+
+if [[ -n "$USE_NAME" ]]; then
+  log "Using Snapshots names"
+  # Cleanup of previous runs
+  cleanup
+
+  log "Creating Symlinks"
+  create_symlinks
+
+  log "Syncing Backup Archives"
+  aws s3 sync "$SYMLINKS_PATH" "s3://$BUCKET/" --quiet
+
+  cleanup
+else
+  log "Continuing without Snapshot names"
+  log "Syncing Backup Archives"
+  aws s3 sync "$BACKUP_PATH" "s3://$BUCKET/" --quiet
+fi
+
+log "Done"
